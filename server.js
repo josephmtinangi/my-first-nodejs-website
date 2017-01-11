f8ae09f3b7f303529c4a87deae9a2d9c20a224ba
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-11 16:31:14 +0300

    Remove deprecated warning

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 // require dependencies
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8080;
 
 // use ejs and express layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// use body parser
+app.use(bodyParser.urlencoded(
+{
+  extended: true
+}
+));
 
 // route our app
 var router = require('./app/routes');
