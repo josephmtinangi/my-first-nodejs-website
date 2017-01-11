96b55591c08097aa3665822ad9c136f9d9bf7740
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-11 10:58:54 +0300

    Directory structure and clean up

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
+// route our app
+var router = require('./app/routes');
+app.use('/', router);
+
 // start the server
 app.listen(port, function() {
   console.log('App started on port ' + port);
 });
 
-// route for home page
-app.get('/', function(req, res) {
-  res.send('hello');
-});
-
-// route for about page
-app.get('/about', function(req, res) {
-  res.send('About page');
-});
