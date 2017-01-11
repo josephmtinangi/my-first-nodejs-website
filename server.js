8936ab0953c1657301b31227a3e51bed3b7e432e
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-11 14:23:19 +0300

    Specify the location of static assets

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var port = 8080;
 var router = require('./app/routes');
 app.use('/', router);
 
+// set the static files
+app.use(express.static(__dirname + '/public'));
+
 // start the server
 app.listen(port, function() {
   console.log('App started on port ' + port);
