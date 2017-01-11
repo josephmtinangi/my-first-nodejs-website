7f2d0984cdba1f0e2ec85656ef4a6582731ea7d4
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-11 10:44:29 +0300

    Create server using express

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-// server.js
-// import the http module
-var http = require('http');
+// // server.js
+// // import the http module
+// var http = require('http');
 
-// handle sending requests and returning response
-function handleRequests(req, res) {
-  // return string
-  res.end('Hello world!');
-}
+// // handle sending requests and returning response
+// function handleRequests(req, res) {
+//   // return string
+//   res.end('Hello world!');
+// }
 
-// create the server
-var server = http.createServer(handleRequests);
+// // create the server
+// var server = http.createServer(handleRequests);
 
-// start server and listen on port x
-server.listen(8080, function() {
-  console.log("Listening on port 8080");
+// // start server and listen on port x
+// server.listen(8080, function() {
+//   console.log("Listening on port 8080");
+// })
+// 
+// 
+
+///////////////////////// EXPRESS //////////////////////
+
+var express = require('express');
+var app = express();
+var port = 8080;
+
+// start the server
+app.listen(port, function() {
+  console.log('App started on port ' + port);
 })
