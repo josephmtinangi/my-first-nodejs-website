479729f90e12a74db95caa604af2e2ccc627bf56
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-11 15:10:15 +0300

    Link pages with the layout

--- app/routes.js
+++ app/routes.js
@@ -10,13 +10,18 @@ module.exports = router;
 
 // route for home page
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../index.html'))
+  res.render('pages/index');
 });
 
 // route for about page
 router.get('/about', function(req, res) {
-  res.sendFile(path.join(__dirname, '../about.html'))
+  res.render('pages/about');
 }); 
 
-router.get('/contact');
-router.post('/contact');
+router.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+router.post('/contact', function(req, res) {
+  
+});
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // require dependencies
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var port = 8080;
+
+// use ejs and express layouts
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // route our app
 var router = require('./app/routes');
 app.use('/', router);
+
 
 // set the static files
 app.use(express.static(__dirname + '/public'));
